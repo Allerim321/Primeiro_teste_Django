eba331ca1e135be664e1465e389f2a1b5213e6a7
Allerim <mirellatcg@gmail.com>
2024-05-14 09:01:00 -0300

    adicionando configurações para adicionar logo

--- My_Site/hotel/models.py
+++ My_Site/hotel/models.py
@@ -11,6 +11,7 @@ TIPOS_QUARTOS = (
 class hotel(models.Model):
     titulo = models.CharField(max_length = 50)
     descricao = models.TextField(max_length = 500)
+    logo = models.ImageField(upload_to='logo/')
     
     def __str__(self):
         return self.titulo
@@ -20,6 +21,7 @@ class quarto(models.Model):
     disponibilidade = models.IntegerField()
     valor = models.FloatField(max_length = 4)
     descricao = models.TextField(max_length = 255)
+    foto_quarto = models.ImageField(upload_to="Foto_Quarto/")
     
     def __str__(self):
         return self.tipo
