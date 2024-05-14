1c457aa4b90af41ee8e87309d55f631e9a39a649
Allerim <mirellatcg@gmail.com>
2024-05-14 11:11:23 -0300

    começando a estilizar

--- My_Site/hotel/views.py
+++ My_Site/hotel/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render, HttpResponse
+from .models import hotel
 
 def homepage(request):
-    return render(request, 'homepage.html')
+    context = {}
+    dados_hotel = hotel.objects.all
+    context['dados_hotel'] = dados_hotel
+    return render(request, 'homepage.html', context)
